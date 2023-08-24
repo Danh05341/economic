47e8c0b65410568923d1cc807785df0de1ab9914
huynh duc truong <khungking909@gmail.com>
2023-08-24 16:21:12 +0700

    4 pull in day 24

---
+++ src/components/MainComponents/index.js
@@ -0,0 +1,12 @@
+import Layout from '../Layout/Layout'
+import './MainComponents.scss'
+const MainComponents = () => {
+    return (
+        <Layout>
+           <div className='container-fluid'>
+                <img className='bg-img' src='https://bizweb.dktcdn.net/100/342/645/themes/701397/assets/slider_2.jpg?1677377108874'/>
+           </div>
+        </Layout>
+    )
+}
+export default MainComponents
