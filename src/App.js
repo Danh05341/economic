af1919a768bb12b264ea5a2784f042334f2a3bf1
huynh duc truong <khungking909@gmail.com>
2023-08-24 16:24:43 +0700

    24 ..

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import Header from './components/Header'
+import MainComponents from './components/MainComponents'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Header/>} />
-        <Route path="/users/new" element={<Header/>} />
-        <Route path="/users/:id" element={<Header/>} />
+        <Route path="/" element={<MainComponents/>} />
+       
       </Routes>
     </Router>
   );
