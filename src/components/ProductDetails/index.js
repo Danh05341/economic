c8e58ee3b763b0abd3f843de55eeff0f5e3ce1c0
Danh05341 <danh05341@gmail.com>
2023-08-27 14:06:32 +0700

    Xây dựng UI trang ProductDetails

--- src/components/ProductDetails/index.js
+++ src/components/ProductDetails/index.js
@@ -1,47 +1,49 @@
 import { BiChevronRight } from "react-icons/bi";
+import { BsFillTelephoneFill } from "react-icons/bs";
 import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
 
+import './ProductDetails.scss'
 function ProductDetail() {
     return (
         <div className="product-detail">
-            <div className="container">
+            <div className="product-container">
                 <div className="bread-crumbs">
-                    <ul className="n">
-                        <li className="">
-                            <a href="#">Trang chủ</a>
-                            <BiChevronRight></BiChevronRight>
+                    <ul className="nav-list">
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Trang chủ</a>
+                            <BiChevronRight className="nav-icon"></BiChevronRight>
                         </li>
-                        <li className="">
-                            <a href="#">Giày nam</a>
-                            <BiChevronRight></BiChevronRight>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Giày nam</a>
+                            <BiChevronRight className="nav-icon"></BiChevronRight>
                         </li>
-                        <li className="">
-                            <a href="#">Giày bóng đá sân cỏ nhân tạo</a>
-                            <BiChevronRight></BiChevronRight>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Giày bóng đá sân cỏ nhân tạo</a>
                         </li>
                     </ul>
-                </div>
-                <div className="product-category-title">
-                    Giày nam
+                    <div className="product-category-title">
+                        Giày nam
+                    </div>
+
                 </div>
                 <div className="product-info">
                     <div className="product-image">
-                        <img src="https://bizweb.dktcdn.net/thumb/small/100/342/645/products/bd30ea8934835dfbad4dca4072b1ba3b.jpg?v=1545572480550"></img>
+                        <img src="https://bizweb.dktcdn.net/thumb/grande/100/342/645/products/bd30ea8934835dfbad4dca4072b1ba3b.jpg?v=1545572480550%22"></img>
                     </div>
                     <div className="product-option">
                         <div className="product-color">
-                            <div className="product-image">
-                                <img src="https://bizweb.dktcdn.net/thumb/small/100/342/645/products/1788475-l.jpg?v=1545572481460"></img>
+                            {/* <div className="product-image">
+                                <img src="https://bizweb.dktcdn.net/thumb/grande/100/342/645/products/bd30ea8934835dfbad4dca4072b1ba3b.jpg?v=1545572480550%22"></img>
                             </div>
                             <div className="product-image">
-                                <img src="https://bizweb.dktcdn.net/thumb/small/100/342/645/products/1788475-l.jpg?v=1545572481460"></img>
+                                <img src="https://bizweb.dktcdn.net/thumb/grande/100/342/645/products/1788475-l.jpg?v=1545572481460"></img>
                             </div>
                             <div className="product-image">
-                                <img src="https://bizweb.dktcdn.net/thumb/small/100/342/645/products/1788475-l.jpg?v=1545572481460"></img>
+                                <img src="https://bizweb.dktcdn.net/thumb/grande/100/342/645/products/0c6ae505cd0029d8918b8a6bebc82fe2.jpg?v=1545572483250"></img>
                             </div>
                             <div className="product-image">
-                                <img src="https://bizweb.dktcdn.net/thumb/small/100/342/645/products/1788475-l.jpg?v=1545572481460"></img>
-                            </div>
+                                <img src="https://bizweb.dktcdn.net/100/342/645/products/giay-da-bong-fg-nguoi-lon-den-trang.jpg?v=1545572483563"></img>
+                            </div> */}
                         </div>
                         <div className="product-bot">
                             <div className="product-name">
@@ -49,7 +51,7 @@ function ProductDetail() {
                             </div>
                             <div className="product-group-status">
                                 <span className="lable-name">
-                                    Thương hiệu: <span className="status-name">Nike</span>
+                                    Thương hiệu: <span className="">Nike</span>
                                 </span>
                                 <span className="lable-name">
                                     Kho: <span className="status-name">Còn hàng</span>
@@ -85,7 +87,8 @@ function ProductDetail() {
                             <div className="product-action">
                                 <button className="product-order">MUA NGAY</button>
                                 <button className="product-contact">
-                                    <span>Mua số lượng lớn</span>
+                                <BsFillTelephoneFill className="product-contact-icon"></BsFillTelephoneFill>
+                                    <p>Mua số lượng lớn</p>
                                     <span>Gọi ngay 19006750</span>
                                 </button>
                             </div>
