f2b2c59b919c65b33a9a7d82a07744436df62d90
huynh duc truong <khungking909@gmail.com>
2023-08-28 08:48:52 +0700

    28/8/2023

--- src/components/MainComponents/index.js
+++ src/components/MainComponents/index.js
@@ -2,6 +2,7 @@ import Layout from '../Layout/Layout'
 import './MainComponents.scss'
 import MainItem from './MainItem'
 import Product from '../Products'
+import { BiChevronRight } from "react-icons/bi";
 const MainComponents = () => {
     let sizeImg = [
         {
@@ -51,7 +52,7 @@ const MainComponents = () => {
                 </div>
                 <div className='selling-container'>
                     <div className='selling'>
-                        <h1 className='selling-title'><a href='#' style={{textDecoration:'none',color:'black'}}>SẢN PHẨM BÁN CHẠY</a></h1>
+                        <h1 className='selling-title'><a href='#' style={{ textDecoration: 'none', color: 'black' }}>SẢN PHẨM BÁN CHẠY</a></h1>
                         <div className='selling-list-item'>
                             <Product />
                             <Product />
@@ -61,6 +62,63 @@ const MainComponents = () => {
                         </div>
                     </div>
 
+                </div>
+                <div className='sneaker-container'>
+                    <div className='sneaker'>
+                        <div className='sneaker-top'>
+                        <img className='sneaker-img' src='https://bizweb.dktcdn.net/100/342/645/themes/701397/assets/banner_product_nangdong.jpg?1677377108874'></img>
+                            <div className='sneaker-right'>
+                                <div className='sneaker-title'>
+                                    <a href='#' >SNEAKER NĂNG ĐỘNG</a>
+                                </div>
+                                <p>Sneaker đã trở thành một biểu tượng của xã hội, là một sản phẩm của thời đại với những thiết kế cổ điển và những điều đó đều nằm trong những đôi giày Sneaker Delta Shoes. Không lỗi thời với thời gian, mang dấu ấn cá tính khác biệt và tạo mọi sự lôi cuốn từ chính đôi giày Sneaker. Tự tạo cuộc chơi, tự tạo phong cách, đó là Delta Shoes</p>
+                                <div className='sneaker-right-btn'><a href='#'>XEM TẤT CẢ <BiChevronRight className='sneaker-right-icon' /> </a >
+                                </div>
+
+                            </div>
+                        </div>
+                        <div className='sneaker-bot'>
+                            <div className='selling-list-item'>
+                                <Product />
+                                <Product />
+                                <Product />
+                                <Product />
+                                <Product />
+                            </div>
+
+
+                        </div>
+
+                    </div>
+                </div>
+                <div className='slip-on-container'>
+                    <div className='slip-on'>
+                        <div className='slip-on-top'>
+                        <div className='slip-on-right'>
+                                <div className='slip-on-title'>
+                                    <a href='#' >SLIP-ON THANH LỊCH</a>
+                                </div>
+                                <p>Ra đời từ thế kỉ XX, từ sự tiện dụng cho đến biểu tượng của phong cách casual. Giày lười Delta Shoes là một trong những đại diện của sự chững chạc trong thời trang, là xu hướng của thế giới thời trang tối giản. Đồng hành cùng Delta Shoes là đồng hành với chuẩn mực của chính bạn.</p>
+                                <div className='slip-on-right-btn'><a href='#'>XEM TẤT CẢ <BiChevronRight className='slip-on-right-icon' /> </a >
+                                </div>
+
+                            </div>
+                            <img className='slip-on-img' src='https://bizweb.dktcdn.net/100/342/645/themes/701397/assets/banner_product_thanhlich.jpg?1677377108874'></img>
+                           
+                        </div>
+                        <div className='slip-on-bot'>
+                            <div className='selling-list-item'>
+                                <Product />
+                                <Product />
+                                <Product />
+                                <Product />
+                                <Product />
+                            </div>
+
+
+                        </div>
+
+                    </div>
                 </div>
 
             </div>
